d4f872f96a7adae4ba983f8ccfa6fc33c37720d0
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2024-09-04 10:40:25 -0600

    Botones y agregar la capacidad de reconocer el btn pulsado

--- LaCalculadoraChede/ViewController.swift
+++ LaCalculadoraChede/ViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     //Declarar Botones
     @IBOutlet weak var botonParaInteractuar: UIButton!
-    
     @IBOutlet weak var TextoCambiar: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-
+//Crear funciones
     @IBAction func QueHacer(_ sender: Any) {
         TextoCambiar.text="Adios Mundo Cruel"
     }
-    
+    //funcion para mostrar el texto de los botones 
+    @IBAction func Opciones(_ sender: Any) {
+        TextoCambiar.text="Aqui van a aparecer las opciones"
+    }
+    @IBAction func Numeros(_ sender: UIButton) {
+        TextoCambiar.text=sender.titleLabel!.text ?? "Nosepo"
+    }
 }
 
