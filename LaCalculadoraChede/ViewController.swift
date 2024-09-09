0ceb986714f0b0ad273a102e55882b86a112ca5a
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2024-09-09 10:38:05 -0600

    Resolver los errores de la calse pasada y hacer nuevos

--- LaCalculadoraChede/ViewController.swift
+++ LaCalculadoraChede/ViewController.swift
@@ -1,58 +1,64 @@
 //
 //  ViewController.swift
-//  LaCalculadoraChede
 //
-//  Created by alumno on 8/30/24.
 //
-
+ 
 import UIKit
-
+enum estados_de_la_calculadora{
+    case seleccionar_nuemros
+    case seleccionar_operacion
+    case mostrar_resultado
+}
+ 
 class ViewController: UIViewController {
-    //Declarar Botones
-    @IBOutlet weak var botonParaInteractuar: UIButton!
-    @IBOutlet weak var TextoCambiar: UILabel!
-    @IBOutlet weak var btn_Operacion: UIButton!
+    var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.selacionar_nuemros
     
+    //Declaracion del boton y el label a interactuar
+    @IBOutlet weak var btn_Cambia: UIButton!
+    @IBOutlet weak var txt_Cambiar: UILabel!
+    @IBOutlet weak var btn_ope: UIButton!
     
-    var boton_interfaz: Dictionary<String, IUBotonCalculadora> =[:]
+    var botones_Interfaz: Dictionary<String, IUBotonCalculadora> = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        IniciarCal()
         // Do any additional setup after loading the view.
+        
+        inicializar_Calculadora()
     }
     
-    //Crear funciones
+    //La declaracion de la accion a hacer y se arrastraron cada boton a esta funcion para que otorgue su numero escrito
     @IBAction func QueHacer(_ sender: UIButton) {
-        //TextoCambiar.text = sender.titleLabel!.text ?? "Nosupe"
-        /*for boton in boton_interfaz{
-            if(boton.id == sender.restorationIdentifier){
-                //funciona como log 
-                print(boton.numero)
-            }
-        }*/
-        print(boton_interfaz[(sender.restorationIdentifier ??
-                              btn_Operacion.restorationIdentifier)?? "boton"]?.numero ?? "Esto no funge")
-    }
-    
-    //funcion para mostrar el texto de los botones
-    @IBAction func Opciones(_ sender: Any) {
-        TextoCambiar.text="Aqui van a aparecer las opciones"
-    }
-    
-    func IniciarCal() -> Void {
-        crear_arreglo_botones()
-    }
-    
-    func crear_arreglo_botones() -> Void{
-        boton_interfaz["btn_\(numero)"] =  IUBotonCalculadora
-        for numero in 0...9{
-            boton_interfaz.append(IUBotonCalculadora(
-                "btn_\(numero)",
-                numero: Character("\(numero)"),
-                operacion: "+"
-                )
-            )
+        if (estado_actual == estados_de_la_calculadora.seleccionar_nuemros){
+            // TODO: Arreglar glitch del text quitando el optional
+            let text_a_añadir = botones_Interfaz[(sender.restorationIdentifier ?? btn_ope.restorationIdentifier) ?? "boton"]?.numero
+            txt_Cambiar.text = "\(txt_Cambiar.text ?? "")\(text_a_añadir!)"
+        }
+        else if (estado_actual == estados_de_la_calculadora.seleccionar_operacion){
+            
         }
     }
+    
+    func inicializar_Calculadora() -> Void{
+        crear_arreglos_botones()
+    }
+    
+    func crear_arreglos_botones() -> Void{
+        botones_Interfaz=IUBotonCalculadora.crear_arreglo()
+        
+    }
+    func dibujar_numeros_u_operaciones(){
+        if(estado_actual == estados_de_la_calculadora.)
+    }
+    
+    //Declaracion del boton operacion
+    @IBAction func btn_Operacion(_ sender: UIButton) {
+        if(estado_actual == estados_de_la_calculadora.seleccionar_nuemros){
+            estado_actual = estados_de_la_calculadora.seleccionar_operacion
+            dibujar_numeros_u_operaciones()
+        }
+        
+    }
+    
 }
+ 
