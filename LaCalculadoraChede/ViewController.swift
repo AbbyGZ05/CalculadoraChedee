838f20b91afa7ba3974eb7ec81ecc22bf989b8bc
alumno <alumno@desktop-1hcfdjv.uacj.mx>
2024-09-11 10:14:01 -0600

    Se creeo la fincion identificador de botones

--- LaCalculadoraChede/ViewController.swift
+++ LaCalculadoraChede/ViewController.swift
@@ -11,14 +11,16 @@ enum estados_de_la_calculadora{
 }
  
 class ViewController: UIViewController {
-    var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.selacionar_nuemros
+    var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_nuemros
     
     //Declaracion del boton y el label a interactuar
     @IBOutlet weak var btn_Cambia: UIButton!
     @IBOutlet weak var txt_Cambiar: UILabel!
     @IBOutlet weak var btn_ope: UIButton!
+    @IBOutlet weak var Stack_view: UIStackView!
     
     var botones_Interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    var operacion_actual: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,7 @@ class ViewController: UIViewController {
         
         inicializar_Calculadora()
     }
+    
     
     //La declaracion de la accion a hacer y se arrastraron cada boton a esta funcion para que otorgue su numero escrito
     @IBAction func QueHacer(_ sender: UIButton) {
@@ -35,12 +38,26 @@ class ViewController: UIViewController {
             txt_Cambiar.text = "\(txt_Cambiar.text ?? "")\(text_a_añadir!)"
         }
         else if (estado_actual == estados_de_la_calculadora.seleccionar_operacion){
-            
+            if let _mensajero: UIButton? = sender{
+                operacion_actual = botones_Interfaz[_mensajero!.restorationIdentifier ?? "boton_0"]?.operacion
+            }
+            else{
+                operacion_actual = nil
+            }
+        }
+    }
+    
+    
+    @IBAction func btn_Operacion(_ sender: UIButton) {
+        if(estado_actual == estados_de_la_calculadora.seleccionar_nuemros){
+            estado_actual = estados_de_la_calculadora.seleccionar_operacion
+            dibujar_numeros_u_operaciones()
         }
     }
     
     func inicializar_Calculadora() -> Void{
         crear_arreglos_botones()
+        identificar_botones()
     }
     
     func crear_arreglos_botones() -> Void{
@@ -48,17 +65,20 @@ class ViewController: UIViewController {
         
     }
     func dibujar_numeros_u_operaciones(){
-        if(estado_actual == estados_de_la_calculadora.)
-    }
-    
-    //Declaracion del boton operacion
-    @IBAction func btn_Operacion(_ sender: UIButton) {
-        if(estado_actual == estados_de_la_calculadora.seleccionar_nuemros){
-            estado_actual = estados_de_la_calculadora.seleccionar_operacion
-            dibujar_numeros_u_operaciones()
+        if(estado_actual == estados_de_la_calculadora.seleccionar_operacion){
         }
-        
     }
-    
+    func identificar_botones(){
+        for pila_de_componentes in Stack_view.subviews{
+            for boton in pila_de_componentes.subviews{
+                print(boton.restorationIdentifier)
+                if let identificador = boton.restorationIdentifier{
+                    botones_Interfaz[identificador]?.referencia_btn_interfaz = boton as? UIButton
+                }
+            }
+        }
+        for elemento in botones_Interfaz.values{
+            elemento.referencia_btn_interfaz?.titleLabel?.text = "h"
+        }
+    }
 }
- 
